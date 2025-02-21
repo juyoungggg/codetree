1de152d53128b1b67e9ab605e8312df8a3d70706
juyoungggg <eun9455@gmail.com>
2025-02-21 16:02:07 +0900

    Title: 전화번호 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250221/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int def, front, back;
+    cin >> def;
+    cin.get();
+    cin >> front;
+    cin.get();
+    cin >> back;
+
+    cout << "010-" << back << "-" << front << endl;
+    
+    return 0;
+}
