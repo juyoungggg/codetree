8132c5096ac54281a91a58bae28b47361cde4c72
juyoungggg <eun9455@gmail.com>
2025-02-21 15:58:43 +0900

    Title: 입력받아 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250221/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long front, back;
+    cin >> front;
+    cin.get();
+    cin >> back;
+
+    cout << front << back << endl;
+    return 0;
+}
