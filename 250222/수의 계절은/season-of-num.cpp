6fb687bd31b71667b903274887d095b5ee1d304a
juyoungggg <eun9455@gmail.com>
2025-02-22 00:04:50 +0900

    Title: 수의 계절은, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250222/수의 계절은/season-of-num.cpp
+++ 250222/수의 계절은/season-of-num.cpp
@@ -4,15 +4,13 @@ using namespace std;
 int main() {
     int m;
     cin >> m;
-    if (1 <= m && m <= 12) {
-        if (3 <= m && m <= 5)
-            cout << "March" << endl;
-        else if (6 <= m && m <= 8)
-            cout << "Summer" << endl;
-        else if (9 <= m && m <= 11)
-            cout << "Fall" << endl;
-        else if ((1 <= m && m <= 2) || m == 12)
-            cout << "Winter" << endl;
-    }
+    if (3 <= m && m <= 5)
+        cout << "Spring" << endl;
+    else if (6 <= m && m <= 8)
+        cout << "Summer" << endl;
+    else if (9 <= m && m <= 11)
+        cout << "Fall" << endl;
+    else if ((1 <= m && m <= 2) || m == 12)
+        cout << "Winter" << endl;
     return 0;
 }
