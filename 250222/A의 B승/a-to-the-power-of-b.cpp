2bd64172bdf76d7d3efc58c87f754e8124bd51e5
juyoungggg <eun9455@gmail.com>
2025-02-22 16:02:15 +0900

    Title: A의 B승, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250222/A의 B승/a-to-the-power-of-b.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int prod = a;
+    for (int i = 1; i < b; i++)
+        prod *= a;
+
+    cout << prod << endl;
+    return 0;
+}
