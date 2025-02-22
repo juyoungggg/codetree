a8c6791e8962addb77f1daacb49679db2e3b9101
juyoungggg <eun9455@gmail.com>
2025-02-22 09:22:57 +0900

    Title: 장학금, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250222/장학금/scholarship.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    if (a >= 90 && b >= 95)
+        cout << 100000 << endl;
+
+    else if (a >= 90 && b >= 90)
+        cout << 50000 << endl;
+        
+    else    
+        cout << 0 << endl;
+
+    return 0;
+}
