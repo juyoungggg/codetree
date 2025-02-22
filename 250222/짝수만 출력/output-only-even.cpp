e3613951260a58936f13469a5c48d4b252a31fc6
juyoungggg <eun9455@gmail.com>
2025-02-22 14:06:33 +0900

    Title: 짝수만 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250222/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    int i = a;
+    while (i <= b) {
+        if (i % 2 == 0)
+            cout << i << " ";
+        i++;
+    }
+    cout << endl;
+    
+    return 0;
+}
