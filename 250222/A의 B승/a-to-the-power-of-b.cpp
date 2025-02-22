707e8005f202c81845a63ba0224c2bd48e341ad0
juyoungggg <eun9455@gmail.com>
2025-02-22 16:02:47 +0900

    Title: A의 B승, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250222/A의 B승/a-to-the-power-of-b.cpp
+++ 250222/A의 B승/a-to-the-power-of-b.cpp
@@ -5,8 +5,8 @@ int main() {
     int a, b;
     cin >> a >> b;
 
-    int prod = a;
-    for (int i = 1; i < b; i++)
+    int prod = 1;
+    for (int i = 0; i < b; i++)
         prod *= a;
 
     cout << prod << endl;
