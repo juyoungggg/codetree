b7c783cb54418bb6399848587ddb74d5369be5fb
juyoungggg <eun9455@gmail.com>
2025-02-22 16:32:03 +0900

    Title: 특정 수 맞히기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250222/특정 수 맞히기/catching-specific-number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    while (1) {
+        cin >> n;
+        if (n < 25)
+            cout << "Higher" << endl;
+        else if (n > 25)
+            cout << "Lower" << endl;
+        else if (n == 25) {
+            cout << "Good" << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
