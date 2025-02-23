9667f4ad152f3a31e3e2febecd8f25a1ca58c4ec
juyoungggg <eun9455@gmail.com>
2025-02-23 18:12:08 +0900

    Title: 별표 출력하기 13, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250223/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < 2 * n; i++) {
+        if (i % 2 == 0) 
+            for (int j = 0; j <= n - i / 2 - 1; j++)
+                cout << "* ";
+            
+        else
+            for (int j = 0; j <= i / 2; j++)
+                cout << "* ";
+
+        cout << endl;
+    }
+
+    return 0;
+}
