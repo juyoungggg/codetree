aeaea024954da8c747dea6f3429cef03fa6451e8
juyoungggg <eun9455@gmail.com>
2025-02-25 17:11:02 +0900

    Title: 동전이 있는 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250225/동전이 있는 위치/Where-coins-are-located.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int arr[n + 1][n + 1] = {0, };
+    
+    int r, c;
+    for (int i = 0; i < m; i++) {
+        cin >> r >> c;
+        arr[r][c] = 1;
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++)
+            cout << arr[i][j] << ' ';
+        cout << endl;
+    }
+
+    return 0;
+}
