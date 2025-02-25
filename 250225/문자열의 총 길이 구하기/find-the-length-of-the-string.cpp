4d44de81ba1c628cbe8203a504bb0a0556a8c2a2
juyoungggg <eun9455@gmail.com>
2025-02-25 17:52:44 +0900

    Title: 문자열의 총 길이 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250225/문자열의 총 길이 구하기/find-the-length-of-the-string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string arr[10];
+    
+    int sum = 0;
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
+        sum += arr[i].length();
+    }
+    cout << sum << endl;
+    
+    return 0;
+}
