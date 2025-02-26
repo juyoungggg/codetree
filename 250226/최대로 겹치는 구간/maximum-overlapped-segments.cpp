2ed46e2cf4b7f69f84ed8e9b8ce832324c1c7257
juyoungggg <eun9455@gmail.com>
2025-02-26 13:40:43 +0900

    Title: 최대로 겹치는 구간, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250226/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 250226/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -11,13 +11,13 @@ int main() {
     int x1, x2;
     for (int i = 0; i < n; i++) {
         cin >> x1 >> x2;
-        for (int j = x1 + OFFSET; j <= x2 + OFFSET; j++)
+        for (int j = x1 + OFFSET; j <= x2 + OFFSET - 1; j++)
             arr[j]++;
     }
 
     int max = 0;
     for (int i = 2; i <= 100 + OFFSET; i++) {
-        if (max <= arr[i])
+        if (max < arr[i])
             max = arr[i];
     }
 
