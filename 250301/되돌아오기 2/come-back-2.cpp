c8c0752c18c68b3c4abf8629c9aa18cfc9949e18
juyoungggg <eun9455@gmail.com>
2025-03-01 10:55:27 +0900

    Title: 되돌아오기 2, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250301/되돌아오기 2/come-back-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int dx[4] = { 0, 1, 0, -1 };
+int dy[4] = { 1, 0, -1, 0 };
+int x = 0, y = 0, dir = 0;
+
+
+int main() {
+    string s;
+    cin >> s;
+
+    int cnt = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 'F') {
+            x += dx[dir];
+            y += dy[dir];
+            cnt++;
+        }
+        else if (s[i] == 'L') {
+            dir = (dir - 1 + 4) % 4;
+            cnt++;
+        }
+        else if (s[i] == 'R') {
+            dir = (dir + 1) % 4;
+            cnt++;
+        }
+
+        if (x == 0 && y == 0) {
+            cout << cnt << endl;
+            return 0;
+        }
+    }
+
+    cout << -1 << endl;
+    return 0;
+}
