ab35ce942872457a0e392a6146d60f0e9f2798ca
juyoungggg <eun9455@gmail.com>
2025-03-02 21:04:48 +0900

    Title: G or H 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250302/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    
+    char arr[10000 + 1] = {};
+    int loc;
+    char sign;
+    int max_idx = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> loc >> sign;
+        arr[loc] = sign;
+        if (max_idx < loc)
+            max_idx = loc;
+    }
+
+    int sum = 0, max = 0;
+    for (int i = 1; i <= max_idx - k; i++) {
+        sum = 0;
+        for (int j = i; j <= i + k; j++) {
+            if (arr[j] == 'G')
+                sum++;
+            else if (arr[j] == 'H')
+                sum += 2;
+        }
+        if (max < sum) 
+            max = sum;
+    }
+
+    cout << max << endl;
+    
+    return 0;
+}
