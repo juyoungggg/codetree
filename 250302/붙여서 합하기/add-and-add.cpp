e7d970506386d6f569eca94ac55569815e0d9a83
juyoungggg <eun9455@gmail.com>
2025-03-02 13:20:12 +0900

    Title: 붙여서 합하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250302/붙여서 합하기/add-and-add.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    int sum = stoi(a + b) + stoi(b + a);
+    cout << sum << endl;
+    return 0;
+}
