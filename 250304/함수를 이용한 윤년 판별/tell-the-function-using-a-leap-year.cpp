21783a597d4c73cddcdf1e5938f639190be3d7c7
juyoungggg <eun9455@gmail.com>
2025-03-04 20:10:32 +0900

    Title: 함수를 이용한 윤년 판별, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250304/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+bool IsLeapYear(int y) {
+    return (y % 4 == 0 && y % 100 != 0) || (y % 400 == 0);
+}
+
+int main() {
+    int y;
+    cin >> y;
+
+    cout << (IsLeapYear(y) ? "true" : "false") << endl;
+    return 0;
+}
