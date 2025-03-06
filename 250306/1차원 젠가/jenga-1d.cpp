a3fc6f75790a0788d2c25278e981c68f6b6cb3f5
juyoungggg <eun9455@gmail.com>
2025-03-06 19:54:26 +0900

    Title: 1차원 젠가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250306/1차원 젠가/jenga-1d.cpp
+++ 250306/1차원 젠가/jenga-1d.cpp
@@ -1,42 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int arr[100 + 1] = {};
-
 int main() {
     int n;
     cin >> n;
 
+    int arr[100 + 1] = {};
     for (int i = 1; i <= n; i++)
         cin >> arr[i];
 
     int temp[100 + 1] = {};
-    int s1, e1, len1;
-    cin >> s1 >> e1;
-    len1 = e1 - s1 + 1;
-    int idx1 = 1;
-    for (int i = 1; i <= n; i++) {
-        if (s1 <= i && i <= e1)
-            continue;
-        temp[idx1++] = arr[i];
+    for (int k = 0; k < 2; k++) {
+        int s, e, len;
+        cin >> s >> e;
+        len = e - s + 1;
+        int idx = 1;
+        for (int i = 1; i <= n; i++) {
+            if (s <= i && i <= e)
+                continue;
+            temp[idx++] = arr[i];
+        }
+        for (int i = 1; i <= n - len; i++)
+            arr[i] = temp[i];
+        n -= len;
     }
-    for (int i = 1; i <= n - len1; i++)
-        arr[i] = temp[i];
 
-    int s2, e2, len2;
-    cin >> s2 >> e2;
-    len2 = e2 - s2 + 1;
-    int idx2 = 1;
-    for (int i = 1; i <= n - len1; i++) {
-        if (s2 <= i && i <= e2)
-            continue;
-        temp[idx2++] = arr[i];
-    }
-    for (int i = 1; i <= n - len1 - len2; i++)
-        arr[i] = temp[i];
-
-    cout << n - len1 - len2 << endl;
-    for (int i = 1; i <= n - len1 - len2; i++)
+    cout << n << endl;
+    for (int i = 1; i <= n; i++)
         cout << arr[i] << endl;
 
     return 0;
