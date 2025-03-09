66f1bdc26a7d2c041b03ed4cc0b66aa1e62a64d1
juyoungggg <eun9455@gmail.com>
2025-03-09 10:15:48 +0900

    Title: 행복한 수열의 개수, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250309/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 250309/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -11,30 +11,35 @@ int main() {
             cin >> arr[i][j];
 
     int cnt, ans = 0;
-    for (int i = 0; i < n; i++) {
-        cnt = 1;
-        for (int j = 1; j < n; j++) {
-            if (arr[i][j] == arr[i][j - 1])
-                cnt++;
-            if (cnt == m) {
-                ans++;   
-                break;
+
+    if (m == 1)
+        ans = 2 * n;
+
+    else {
+        for (int i = 0; i < n; i++) {
+            cnt = 1;
+            for (int j = 1; j < n; j++) {
+                if (arr[i][j] == arr[i][j - 1])
+                    cnt++;
+                if (cnt == m) {
+                    ans++;   
+                    break;
+                }
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            cnt = 1;
+            for (int i = 1; i < n; i++) {
+                if (arr[i - 1][j] == arr[i][j])
+                    cnt++;
+                if (cnt == m) {
+                    ans++;
+                    break;
+                }
             }
         }
     }
-
-    for (int j = 0; j < n; j++) {
-        cnt = 1;
-        for (int i = 1; i < n; i++) {
-            if (arr[i - 1][j] == arr[i][j])
-                cnt++;
-            if (cnt == m) {
-                ans++;
-                break;
-            }
-        }
-    }
-
     cout << ans << endl;
     return 0;
 }
