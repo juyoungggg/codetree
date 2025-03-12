cd378a22b34a7d15c1ba4141ff43738050b8096b
juyoungggg <eun9455@gmail.com>
2025-03-12 21:00:15 +0900

    Title: Factorial, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250312/Factorial/factorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int Fact(int n) {
+    if (n == 1)
+        return 1;
+    return n * Fact(n - 1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    cout << Fact(n) << endl;
+    return 0;
+}
