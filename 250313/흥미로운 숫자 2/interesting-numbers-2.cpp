e9014750818d3b92263cacae42d55aa78242e8fb
juyoungggg <eun9455@gmail.com>
2025-03-13 19:48:45 +0900

    Title: 흥미로운 숫자 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250313/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool Magic(int n) {
+    int arr[10];
+    int idx = 0;
+
+    while (n != 0) {
+        arr[idx++] = n % 10;
+        n /= 10;
+    }
+
+    sort(arr, arr + idx);
+
+    int cnt = 1;
+    for (int i = 1; i < idx; i++) 
+        if (arr[i - 1] != arr[i])
+            cnt++;
+    if (cnt == 2)
+        return true;
+    else
+        return false;
+}
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+
+    int ans = 0;
+    for (int i = x; i <= y; i++)
+        if (Magic(i))
+            ans++;
+
+    cout << ans << endl;
+    return 0;
+}
+
+/*
+141
+1411
+11411
+*/
