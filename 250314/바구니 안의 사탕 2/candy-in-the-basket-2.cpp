07d5d87e1af861a43f828027f9c50163d1a0b352
juyoungggg <eun9455@gmail.com>
2025-03-14 16:42:00 +0900

    Title: 바구니 안의 사탕 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250314/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    int arr[100 + 1] = {};
+
+    int candy, basket;
+    for (int i = 0; i < n; i++) {
+        cin >> candy >> basket;
+        arr[basket] += candy;
+    }
+
+    int sum, ans = 0;
+    if (k < 50) {
+        for (int c = k; c <= 100 - k; c++) {
+            sum = 0;
+            for (int j = c - k; j <= c + k; j++)
+                sum += arr[j];
+            if (ans < sum)
+                ans = sum;
+        }
+    }
+
+    else if (k >= 50) {
+        for (int i = 0; i < 100; i++)
+            ans += arr[i];
+    }
+
+    cout << ans << endl;
+    return 0;
+}
