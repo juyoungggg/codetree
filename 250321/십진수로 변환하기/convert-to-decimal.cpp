c115fe50816ee553c84c1dd2c62104be395d776d
juyoungggg <eun9455@gmail.com>
2025-03-21 15:23:58 +0900

    Title: 십진수로 변환하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250321/십진수로 변환하기/convert-to-decimal.cpp
+++ 250321/십진수로 변환하기/convert-to-decimal.cpp
@@ -4,25 +4,43 @@ using namespace std;
 int digits[10] = {};
 int cnt = 0;
 
-void integer_to_digits(int n) {
+void number_to_digits(int n) {
     if (n < 10) {
         digits[cnt++] = n % 10;
         return;
     }
-    integer_to_digits(n / 10);
+    number_to_digits(n / 10);
     digits[cnt++] = n % 10;
 }
+
+int digits2[10] = {};
+int cnt2 = 0;
+
+void decimal_to_binary(int n) {
+    if (n < 2) {
+        digits2[cnt2++] = n % 2;
+        return;
+    }
+    decimal_to_binary(n / 2);
+    digits2[cnt2++] = n % 2;
+}
+
 
 int main() {
     int n;
     cin >> n;
 
-    integer_to_digits(n);
+    number_to_digits(n);
     
-    int ans = 0;
-    for (int i = 0; i < cnt; i++) 
-        ans = ans * 2 + digits[i];
+    int decimal = 0;
+    for (int i = 0; i < cnt; i++)
+        decimal = decimal * 2 + digits[i];
 
-    cout << ans << endl;
+    decimal *= 17;
+    decimal_to_binary(decimal);
+    for (int i = 0; i < cnt2; i++)
+        cout << digits2[i];
+    
+    cout << endl;
     return 0;
 }
