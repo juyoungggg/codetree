bda047ae12979e3df20c1e7dd2ed42466eeac61c
juyoungggg <eun9455@gmail.com>
2025-03-25 13:24:43 +0900

    Title: 이상한 진수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250325/이상한 진수 2/awkward-digits-2.cpp
+++ 250325/이상한 진수 2/awkward-digits-2.cpp
@@ -5,17 +5,21 @@ using namespace std;
 int main() {
     string str;
     cin >> str;
+    int len = str.length();
 
-    for (int i = str.length() - 1; i >= 0; i--)
-        if (str[i] == 0) {
-            str[i] = 1;
+    for (int i = 0; i < len; i++)
+        if (str[i] == '0') {
+            str[i] = '1';
             break;
         }
-    cout << str << endl;
+
+    int arr[len];
+    for (int i = 0; i < len; i++)
+        arr[i] = (int)(str[i] - '0');
 
     int ans = 0;
-    for (int i = 0; i < str.length(); i++)
-        ans = ans * 2 + (int)(str[i]);
+    for (int i = 0; i < len; i++)
+        ans = ans * 2 + arr[i];
 
     cout << ans << endl;
     return 0;
