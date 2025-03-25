ea6de0673d9311f12437ae70451c2b9cf495997e
juyoungggg <eun9455@gmail.com>
2025-03-25 13:30:10 +0900

    Title: 이상한 진수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250325/이상한 진수 2/awkward-digits-2.cpp
+++ 250325/이상한 진수 2/awkward-digits-2.cpp
@@ -6,13 +6,22 @@ int main() {
     string str;
     cin >> str;
     int len = str.length();
+    bool flag = false;
 
     for (int i = 0; i < len; i++)
         if (str[i] == '0') {
             str[i] = '1';
+            flag = true;
             break;
         }
 
+    if (!flag) {
+        for (int i = len - 1; i >= 0; i--)
+            if (str[i] == '1') {
+                str[i] = '0';
+                break;
+            }
+    }
     int arr[len];
     for (int i = 0; i < len; i++)
         arr[i] = (int)(str[i] - '0');
