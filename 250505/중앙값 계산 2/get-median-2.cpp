bb04727499dfdca4746c8f4c8d95b550c9cde7c0
juyoungggg <eun9455@gmail.com>
2025-05-05 20:30:59 +0900

    Title: 중앙값 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250505/중앙값 계산 2/get-median-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if (i % 2 == 0) {
+            sort(arr, arr + i + 1);
+            cout << arr[i / 2] << ' ';
+        }
+    }
+    cout << endl;
+    return 0;
+}
