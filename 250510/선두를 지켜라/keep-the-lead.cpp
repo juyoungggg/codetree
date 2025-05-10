1ef5f4144114c52bb041d5088090f60c39799563
juyoungggg <eun9455@gmail.com>
2025-05-10 14:13:17 +0900

    Title: 선두를 지켜라, Time: 13ms, Memory: 9MB, Status: Passed - Codetree

--- 250510/선두를 지켜라/keep-the-lead.cpp
+++ 250510/선두를 지켜라/keep-the-lead.cpp
@@ -31,15 +31,31 @@ int main() {
         }
     }
 
-    int tmp = 0, cnt = 0;
-    for (int i = 1; i < a_time; i++) {
-        if (tmp * (a[i] - b[i]) < 0)
-            cnt++;
+    // for (int i = 0; i < a_time; i++) 
+    //     cout << a[i] << ' ' << b[i] << endl;
 
-        if (a[i] != b[i])
-            tmp = a[i] - b[i];
+    int track[a_time] = {0, };
+    // track의 원소가 0이면 공동 선두, 1이면 a가 선두, -1이면 b가 선두
+    for (int i = 1; i < a_time; i++) {
+        if (a[i] > b[i]) track[i] = 1;
+        else if (a[i] == b[i]) track[i] = 0;
+        else if (a[i] < b[i]) track[i] = -1;
     }
 
+    // for (int i = 0; i < a_time; i++)
+    //     cout << track[i] << ' ';
+    // cout << endl;
+
+    int tmp = 0, cnt = 0;
+    for (int i = 1; i < a_time; i++) {
+        if (tmp * track[i] == -1) {
+            cnt++;
+            // cout << i << ' ';
+        }
+        if (track[i] != 0)
+            tmp = track[i];
+    }
+        
     cout << cnt << endl;
     return 0;
 }
