c5e0fe0b326ffcb009fd3c87f3e67defb2145b35
juyoungggg <eun9455@gmail.com>
2025-05-18 20:43:41 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250518/두 선분/two-lines.cpp
+++ 250518/두 선분/two-lines.cpp
@@ -5,10 +5,10 @@ int main() {
     int x1, x2, x3, x4;
     cin >> x1 >> x2 >> x3 >> x4;
 
-    if (!(x2 < x3 && x4 < x1))
-        cout << "intersecting" << endl;
-    else
+    if (x2 < x3 || x4 < x1)
         cout << "notintersecting" << endl;
+    else
+        cout << "intersecting" << endl;
     
     return 0;
 }
