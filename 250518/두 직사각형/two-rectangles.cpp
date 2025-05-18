a6a4c0c13feeb9dea1754f772ca11f8b91733139
juyoungggg <eun9455@gmail.com>
2025-05-18 20:50:04 +0900

    Title: 두 직사각형, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250518/두 직사각형/two-rectangles.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x1, y1, x2, y2;
+    int a1, b1, a2, b2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    cin >> a1 >> b1 >> a2 >> b2;
+
+    if (x2 < a1 || a2 < x1 || b2 < y1 || y2 < b1)
+        cout << "nonoverlapping" << endl;
+    else
+        cout << "overlapping" << endl;
+
+    return 0;
+}
