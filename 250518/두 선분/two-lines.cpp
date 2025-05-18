aafbbd9ee1544b6f50c79978ce2f6e94c17104da
juyoungggg <eun9455@gmail.com>
2025-05-18 20:44:24 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250518/두 선분/two-lines.cpp
+++ 250518/두 선분/two-lines.cpp
@@ -6,7 +6,7 @@ int main() {
     cin >> x1 >> x2 >> x3 >> x4;
 
     if (x2 < x3 || x4 < x1)
-        cout << "notintersecting" << endl;
+        cout << "nonintersecting" << endl;
     else
         cout << "intersecting" << endl;
     
