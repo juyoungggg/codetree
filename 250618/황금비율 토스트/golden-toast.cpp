7cd367ddf73562c9aaed03e31b5ed10bdbb18c11
juyoungggg <eun9455@gmail.com>
2025-06-18 14:41:10 +0900

    Title: 황금비율 토스트, Time: 19ms, Memory: 1MB, Status: Runtime Error - Codetree

---
+++ 250618/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+    int n, m;
+    list<char> l;
+
+    cin >> n >> m;
+    char c;
+    for (int i = 0; i < n; i++) {
+        cin >> c;
+        l.push_back(c);
+    }
+
+    list<char>:: iterator it;    // 빵이 가리키는 위치
+    it = l.end();               // 처음에는 맨 끝에 위치
+    for (int i = 0; i < m; i++) {
+        cin >> c;
+        if (c == 'L' && it != l.begin())
+            it--;
+
+        else if (c == 'R' && it != l.end())
+            it++;
+
+        else if (c == 'D' && it != l.end())
+            l.erase(it);
+
+        else if (c == 'P') {
+            char nc;
+            cin >> nc;
+            l.insert(it, nc);
+        }
+    }
+
+    for (it = l.begin(); it != l.end(); it++) 
+        cout << *it;
+    cout << endl;
+
+    return 0;
+}
