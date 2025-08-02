c721097bb90d18f681c6091c4e1aaa210a692479
juyoungggg <eun9455@gmail.com>
2025-08-03 00:03:56 +0900

    Title: 아름다운 수열 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250803/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int arr1[n] = {};
+    int arr2[m] = {};
+
+    for (int i = 0; i < n; i++)
+        cin >> arr1[i];
+    for (int i = 0; i < m; i++)
+        cin >> arr2[i];
+    
+    sort(arr2, arr2 + m);
+
+    int ans = 0;
+    for (int i = 0; i < n - m + 1; i++) {
+        int tmp[m] = {};
+        for (int j = i; j < i + m; j++) 
+            tmp[j - i] = arr1[j];
+        sort(tmp, tmp + m);
+        
+        int k;
+        for (k = 0; k < m; k++) {
+            if (arr2[k] != tmp[k])
+                break;
+        }
+        if (k == m)
+            ans++;
+    }
+
+    cout << ans << endl;
+    return 0;
+}
