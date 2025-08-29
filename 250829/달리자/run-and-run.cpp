3b8f0209a3b65a338620af4c3228c2af3fb31986
juyoungggg <eun9455@gmail.com>
2025-08-29 15:15:40 +0900

    Title: 달리자, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250829/달리자/run-and-run.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[n] = {0, };
+    int b[n] = {0, };
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+        cin >> b[i];
+
+    
+    
+    return 0;
+}
